933a72b134daf587dd22140069b76365060aacb8
IntangibleMatter <47702559+IntangibleMatter@users.noreply.github.com>
2024-11-17 18:54:25 -0800

    droplets!

--- src/components/WaterDrop.tsx
+++ src/components/WaterDrop.tsx
@@ -1,3 +1,39 @@
-import { Shape } from "@motion-canvas/2d";
+import { Circle, Line, Rect, Shape, ShapeProps } from "@motion-canvas/2d";
+import { colour_fg } from "../defs/theme";
+import {
+	createRef,
+	createRefArray,
+	easeInCubic,
+	easeOutCubic,
+	easeOutQuint,
+	sequence,
+	Vector2,
+} from "@motion-canvas/core";
 
-export class WaterDrop extends Shape {}
+export class WaterDrop extends Shape {
+	private readonly drop = createRef<Circle>();
+	private readonly concentric = createRefArray<Circle>();
+
+	public constructor(props?: ShapeProps) {
+		if (!props.fill) {
+			props.fill = colour_fg;
+		}
+		super({ ...props });
+		this.add(<Circle ref={this.drop} />);
+	}
+
+	public *dropAt(position: Vector2) {
+		this.drop().position([position.x, -640]);
+		yield* this.drop().position(position, 0.167, easeInCubic);
+		this.drop().opacity(0);
+		sequence(
+			0.0667,
+			...this.concentric.map(function* (con) {
+				con.position(position);
+				con.size([0, 0]);
+				yield con.size([128, 128], 0.25, easeOutCubic);
+				yield con.opacity(0, 0.25, easeOutQuint);
+			}),
+		);
+	}
+}
--- src/scenes/00_intro.tsx
+++ src/scenes/00_intro.tsx
@@ -1,28 +1,41 @@
 import { Circle, makeScene2D, Rect } from "@motion-canvas/2d";
-import { createRef, createRefArray, waitFor, waitUntil } from "@motion-canvas/core";
+import {
+	createRef,
+	createRefArray,
+	useLogger,
+	usePlayback,
+	waitFor,
+	waitUntil,
+} from "@motion-canvas/core";
 import { colour_bg } from "../defs/theme";
 import { WaterDrop } from "../components/WaterDrop";
 
 interface DropTimePair {
-	drop?: WaterDrop;
+	drop: WaterDrop;
 	wait: number;
 }
-
-const droplets: DropTimePair[] = [
-	{ drop: null, wait: 0 }, // also on 0.4 second wait
-	{ drop: null, wait: 0.15 },
-	{ drop: null, wait: 0.39 },
-	{ drop: null, wait: 0.39 },
-	{ drop: null, wait: 0.16 },
-	{ drop: null, wait: 0.39 },
-	{ drop: null, wait: 0.31 },
-	{ drop: null, wait: 0.31 },
-];
 
 export default makeScene2D(function* (view) {
 	const bg = createRef<Rect>();
 
+	const droplets: DropTimePair[] = [
+		{ drop: new WaterDrop({ fill: "#ff0000", x: 0, y: 0 }), wait: 0 }, // also on 0.4 second wait
+		{ drop: new WaterDrop({ fill: "#00ff00", x: 0, y: 0 }), wait: 0.15 },
+		{ drop: new WaterDrop({ fill: "#0000ff", x: 0, y: 0 }), wait: 0.39 },
+		{ drop: new WaterDrop({ fill: "#ffff00", x: 0, y: 0 }), wait: 0.39 },
+		{ drop: new WaterDrop({ fill: "#ff00ff", x: 0, y: 0 }), wait: 0.16 },
+		{ drop: new WaterDrop({ fill: "#123456", x: 0, y: 0 }), wait: 0.39 },
+		{ drop: new WaterDrop({ fill: "#f05090", x: 0, y: 0 }), wait: 0.31 },
+		{ drop: new WaterDrop({ fill: "#069420", x: 0, y: 0 }), wait: 0.31 },
+	];
+
 	view.add(<Rect ref={bg} fill={colour_bg} size={[1920, 1080]} />);
+	for (const drop of droplets) {
+		view.add(drop.drop);
+	}
+	const logger = useLogger();
+	const playback = usePlayback();
+	const ndrop = createRef<WaterDrop>();
 
 	// TODO: calculate timings of beats
 	//
@@ -32,16 +45,17 @@ export default makeScene2D(function* (view) {
 	// but also "over" a piece of water. REALLY close up tho
 	//
 	for (let i = 0; i < 4; i++) {
-		if (i > 0) {
-			yield* waitFor(0.4);
-		}
-
-		droplets[0] = { wait: 0 };
 		for (const drop of droplets) {
 			yield* waitFor(drop.wait);
-			yield drop.drop?.position.y(0, 0); // replace with the actual drop doing its thing call
+			drop.drop.moveToTop();
+			logger.debug(
+				`Waited: ${drop.wait}s for bg of colour ${drop.drop.fill()} on frame ${playback.frame}.`,
+			);
+			yield drop.drop.position.y(0, 0); // replace with the actual drop doing its thing call
 		}
+		yield* waitFor(0.4);
+		logger.debug(`BONUS WAIT OF 0.4s FOR CYCLE OFFSET OF ${playback.frame}`);
 	}
 
-	yield* waitUntil("end");
+	//yield* waitUntil("end");
 });
